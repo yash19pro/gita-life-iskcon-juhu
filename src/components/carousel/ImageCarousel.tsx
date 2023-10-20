e0c99826ba8730f74bf60b51b7c45510823259ee
Yash Patel <yash19.pro@gmail.com>
2023-10-20 22:17:17 +0530

    adding new images

--- src/components/carousel/ImageCarousel.tsx
+++ src/components/carousel/ImageCarousel.tsx
@@ -6,72 +6,51 @@ import Image from "next/image";
 
 const images = [
     {
-        id: 7,
-        src: "/carousel-images/talasari-7.jpeg",
-        alt: "Talasari Camp",
+        src: "/carousel-images/lord-vishnu-and-durga-devi-navaratri.jpg",
+        alt: "Lord Vishnu and Durga Devi",
     },
     {
-        id: 1,
-        src: "/carousel-images/talasari-1.jpeg",
-        alt: "Talasari Camp",
+        src: "/carousel-images/harinaam-week-1.jpeg",
+        alt: "Harinaam",
     },
     {
-        id: 2,
-        src: "/carousel-images/talasari-2.jpeg",
-        alt: "Talasari Camp",
+        src: "/carousel-images/harinaam-week-2.jpeg",
+        alt: "Harinaam",
     },
     {
-        id: 3,
+        src: "/carousel-images/harinaam-week-3.jpeg",
+        alt: "Harinaam",
+    },
+    {
         src: "/carousel-images/talasari-3.jpeg",
         alt: "Talasari Camp",
     },
     {
-        id: 4,
         src: "/carousel-images/talasari-4.jpeg",
         alt: "Talasari Camp",
     },
     {
-        id: 5,
-        src: "/carousel-images/talasari-5.jpeg",
-        alt: "Talasari Camp",
-    },
-    {
-        id: 6,
-        src: "/carousel-images/talasari-6.jpeg",
-        alt: "Talasari Camp",
-    },
-    {
-        id: 8,
         src: "/carousel-images/radhaashtami-1.jpeg",
         alt: "Radha Ashtami",
     },
     {
-        id: 9,
         src: "/carousel-images/radhaashtami-2.jpeg",
         alt: "Radha Ashtami",
     },
+
     {
-        id: 10,
         src: "/carousel-images/radhaashtami-3.jpeg",
         alt: "Radha Ashtami",
     },
     {
-        id: 11,
         src: "/carousel-images/radhaashtami-4.jpeg",
         alt: "Radha Ashtami",
     },
     {
-        id: 12,
-        src: "/carousel-images/radhaashtami-5.jpeg",
-        alt: "Radha Ashtami",
-    },
-    {
-        id: 13,
         src: "/carousel-images/radhaashtami-6.jpeg",
         alt: "Radha Ashtami",
     },
     {
-        id: 14,
         src: "/carousel-images/radhaashtami-7.jpeg",
         alt: "Radha Ashtami",
     },
@@ -88,8 +67,8 @@ const ImageCarousel = () => {
                 interval={5000} // Auto-advance every 5 seconds
                 transitionTime={500} // Animation duration
             >
-                {images.map((image) => (
-                    <div key={image.id}>
+                {images.map((image, index) => (
+                    <div key={index}>
                         <Paper elevation={3} className="carousel-image">
                             <Image
                                 src={image.src}
