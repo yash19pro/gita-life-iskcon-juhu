3a8be87b77c7ccb4cbafb081f21f39e07d6ce0be
Yash Patel <yash19.pro@gmail.com>
2023-11-15 12:32:58 +0530

    adding giriraj ji's photos

--- src/components/data/images.ts
+++ src/components/data/images.ts
@@ -3,10 +3,41 @@ const images = [
         src: "/carousel-images/prabhupada-disappearance.jpeg",
         alt: "Prabhupada Disappearance Day",
     },
-
     {
         src: "/carousel-images/kartik-maas.jpeg",
         alt: "Kartik Maas",
+    },
+    {
+        src: "/carousel-images/giriraj-1.jpeg",
+        alt: "Giriraj Ji",
+    },
+    {
+        src: "/carousel-images/giriraj-2.jpeg",
+        alt: "Giriraj Ji",
+    },
+    {
+        src: "/carousel-images/giriraj-3.jpeg",
+        alt: "Giriraj Ji",
+    },
+    {
+        src: "/carousel-images/giriraj-4.jpeg",
+        alt: "Giriraj Ji",
+    },
+    {
+        src: "/carousel-images/giriraj-5.jpeg",
+        alt: "Giriraj Ji",
+    },
+    {
+        src: "/carousel-images/giriraj-6.jpeg",
+        alt: "Giriraj Ji",
+    },
+    {
+        src: "/carousel-images/giriraj-7.jpeg",
+        alt: "Giriraj Ji",
+    },
+    {
+        src: "/carousel-images/giriraj-8.jpeg",
+        alt: "Giriraj Ji",
     },
     {
         src: "/carousel-images/gev-1.jpeg",
@@ -17,19 +48,7 @@ const images = [
         alt: "GEV camp",
     },
     {
-        src: "/carousel-images/gev-3.jpeg",
-        alt: "GEV camp",
-    },
-    {
-        src: "/carousel-images/gev-4.jpeg",
-        alt: "GEV camp",
-    },
-    {
-        src: "/carousel-images/gev-6.jpeg",
-        alt: "GEV camp",
-    },
-    {
-        src: "/carousel-images/talasari-1.jpeg",
+        src: "/carousel-images/talasari-5.jpeg",
         alt: "Talasari camp",
     },
     {
@@ -37,32 +56,7 @@ const images = [
         alt: "Talasari camp",
     },
     {
-        src: "/carousel-images/talasari-3.jpeg",
-        alt: "Talasari camp",
-    },
-    {
-        src: "/carousel-images/talasari-4.jpeg",
-        alt: "Talasari camp",
-    },
-    {
-        src: "/carousel-images/talasari-5.jpeg",
-        alt: "Talasari camp",
-    },
-    {
         src: "/carousel-images/talasari-6.jpeg",
-        alt: "Talasari camp",
-    },
-    {
-        src: "/carousel-images/talasari-7.jpeg",
-        alt: "Talasari camp",
-    },
-    {
-        src: "/carousel-images/talasari-8.jpeg",
-        alt: "Talasari camp",
-    },
-
-    {
-        src: "/carousel-images/talasari-10.jpeg",
         alt: "Talasari camp",
     },
 ];
